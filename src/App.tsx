822bb8d9a4e4c2c92b8b3a9cc689606b9acc20d9
Ali-Pixeled <ali.chitral50@gmail.com>
2024-11-10 22:29:03 +0500

    Update 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 // src/App.tsx
 
-import React from "react";
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { config } from "./config/wagmi";
